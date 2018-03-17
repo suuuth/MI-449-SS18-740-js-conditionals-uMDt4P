45f3b2503d9c48bc6f5c77e7791179ebadccfcc7
suuuth <suther98@msu.edu>
2018-03-17 15:40:01 -0400

    js lint errors corrected

--- js.js
+++ js.js
@@ -1,27 +1,27 @@
 
 var a = window.prompt('something happened!  What will you do?')
 if (a === null || a === undefined || a === '') {
-  alert('Not an option, try again!')
-  var a = window.prompt('something happened!  What will you do?')
+  window.alert('Not an option, try again!')
+  a = window.prompt('something happened!  What will you do?')
   partOne(a)
 } else {
   a = a.toLowerCase().trim()
   partOne(a)
 }
 
-function partOne(choice) {
+function partOne (choice) {
   if (choice === 'something good') {
-      partTwo_good()
+    PartTwoGood()
   } else if (choice === 'something average') {
-    partTwo_avg(b)
+    PartTwoAvg()
   } else if (choice === 'something bad') {
-    var b = confirm('that was a poor choice!  Do you want to try again?')
+    var b = window.confirm('that was a poor choice!  Do you want to try again?')
     _Confirm(b)
   } else {
     window.alert('not an option! Try Again')
-    var b = window.prompt('something happened!  What will you do?')
+    b = window.prompt('something happened!  What will you do?')
     if (b === null || b === undefined || b === '') {
-      alert('Not an option, try again!')
+      window.alert('Not an option, try again!')
       var a = window.prompt('something happened!  What will you do?')
       partOne(a)
     } else {
@@ -31,75 +31,75 @@ function partOne(choice) {
   }
 }
 
-function partTwo_good() {
+function PartTwoGood () {
   var b = window.prompt('Something good happened!  What will you do next?')
   if (b === null || b === undefined || b === '') {
-    alert('Not an option, try again!')
-    var b = window.prompt('Something good happened!  What will you do next?')
+    window.alert('Not an option, try again!')
+    b = window.prompt('Something good happened!  What will you do next?')
   } else {
-    game_ending(b)
+    GameEnding(b)
   }
 }
 
-function partTwo_avg() {
+function PartTwoAvg () {
   var b = window.prompt('Something happens, neither good nor bad.  Now what will you do?')
   if (b === null || b === undefined || b === '') {
-    alert('Not an option, try again!')
-    var b = window.prompt('Something happens, neither good nor bad.  Now what will you do?')
+    window.alert('Not an option, try again!')
+    b = window.prompt('Something happens, neither good nor bad.  Now what will you do?')
   } else {
-    game_ending()
+    GameEnding()
   }
 }
 
-function game_ending() {
+function GameEnding () {
   var c = Math.random()
   var d = window.prompt('The devil himself appears and asks you what number he is thinking of.  He says \'The number is either 1 or 0!  Guess wrong and you can never leave!  Guess right and you may find the way out of this maze of js prompts!\'')
   if (isNaN(d)) {
-    alert('try again')
-    game_ending()
+    window.alert('try again')
+    GameEnding()
   } else {
-    d = parseInt(''+d)
+    d = parseInt('' + d)
   }
 
-  if (d != 0 || d != 1) {
-    var e = confirm('that was a poor choice!  Do you want to try again?')
+  if (d !== 0 || d !== 1) {
+    var e = window.confirm('that was a poor choice!  Do you want to try again?')
     _Confirm(e)
   } else if (c < 0.5 && d === 0) {
-    alert('you win the game!')
-    var e = confirm('play again?')
+    window.alert('you win the game!')
+    e = window.confirm('play again?')
     _Confirm(e)
   } else if (c > 0.5 && c === 1) {
-    alert('you win the game!')
-    var e = confirm('play again?')
+    window.alert('you win the game!')
+    e = window.confirm('play again?')
     _Confirm(e)
   } else if (c < 0.5 && c === 1) {
-    alert('you lose the game!')
-    var e = confirm('play again?')
+    window.alert('you lose the game!')
+    e = window.confirm('play again?')
     _Confirm(e)
   } else if (c > 0.5 && c === 0) {
-    alert('you lose the game!')
-    var e = confirm('play again?')
+    window.alert('you lose the game!')
+    e = window.confirm('play again?')
     _Confirm(e)
   }
 }
 
-function _Confirm(choice) {
-  if (choice == true) {
-    alert('Good Choice')
+function _Confirm (choice) {
+  if (choice === true) {
+    window.alert('Good Choice')
     var a = window.prompt('something happened!  What will you do?')
     if (a === null || a === undefined) {
-      alert('Not an option, try again!')
-      var a = window.prompt('something happened!  What will you do?')
+      window.alert('Not an option, try again!')
+      a = window.prompt('something happened!  What will you do?')
     } else {
       a = a.toLowerCase().trim()
       partOne(a)
     }
   } else {
-    alert('Game Over')
-    var a = window.prompt('something happened!  What will you do?')
+    window.alert('Game Over')
+    a = window.prompt('something happened!  What will you do?')
     if (a === null || a === undefined) {
-      alert('Not an option, try again!')
-      var a = window.prompt('something happened!  What will you do?')
+      window.alert('Not an option, try again!')
+      a = window.prompt('something happened!  What will you do?')
     } else {
       a = a.toLowerCase().trim()
       partOne(a)
